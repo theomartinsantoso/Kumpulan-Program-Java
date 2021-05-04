7f913b29463bfb2a5e0f64f27993e14d857a55ae
Theo Martin Santoso <79010034+theomartinsantoso@users.noreply.github.com>
2021-05-04 23:43:41 +0700

    Create PersegiPanjang.java

---
+++ PersegiPanjang.java
@@ -0,0 +1,14 @@
+class PersegiPanjang {
+    // untuk menghitung luas persegi panjang
+    public static double luas(double panjang, 
+                            double lebar) {
+        return panjang * lebar;
+    }
+    
+    // untuk menghitung keliling persegi panjang
+    public static double keliling(double panjang, 
+                                  double lebar) {
+        return 2 * (panjang + lebar);
+    }
+}
+     
