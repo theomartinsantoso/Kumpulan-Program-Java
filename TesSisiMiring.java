711b98bc0ad15476ed18ed0089f3ef6a1ea59c93
Theo Martin Santoso <79010034+theomartinsantoso@users.noreply.github.com>
2021-05-04 23:46:57 +0700

    Create TesSisiMiring.java

---
+++ TesSisiMiring.java
@@ -0,0 +1,13 @@
+class TesSisiMiring {
+    public static void main(String[] args) {
+        double x, y, z;
+        
+        x = 4;
+        y = 3;
+        z = SegitigaSiku.sisiMiring(x, y);
+        System.out.println("Sisi A: " + x);
+        System.out.println("Sisi B: " + y);
+        System.out.println("Sisi miring: " + z);
+    }
+}
+     
