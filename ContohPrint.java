0224f22c1d04ee6654accd7f450e8ace74d786ff
Theo Martin Santoso <79010034+theomartinsantoso@users.noreply.github.com>
2021-05-04 23:14:08 +0700

    Create ContohPrint.java

---
+++ ContohPrint.java
@@ -0,0 +1,14 @@
+class ContohPrint {
+    public static void main(String[] args) {
+    int tanggal = 17;
+		int a,b;
+	  String bulan = "Agustus";
+ 
+        a=b=0;
+        System.out.print(tanggal);
+	      System.out.print(bulan);
+	      System.out.println();
+        System.out.println(tanggal);
+	    System.out.println(bulan);
+    }
+}
